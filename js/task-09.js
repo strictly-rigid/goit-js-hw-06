e4ab12ef75914ba0fc9f45de1e319d839eb47dac
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-04-28 22:54:48 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+const handleChangeColor = function (event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
+
+buttonEl.addEventListener('click', handleChangeColor);
+
