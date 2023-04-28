75073bb756e473775d5506ef90e4ad7f5126a496
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-04-28 21:52:30 +0300

    tasks 6-7 done

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,13 @@
 const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', () => {
+    if (inputEl.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
+  }
+});
+
+
