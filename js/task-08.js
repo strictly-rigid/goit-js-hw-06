87e033002bf8aa49054601e66da4a45c82c8bcde
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-04-28 22:17:56 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', handleFormSubmit)
+
+const handleFormSubmit = function (event) {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "")
+    { return alert("Заповніть всі поля!"); }
+
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+    email,
+    password
+  };
+
+    console.log(formData);
+
+    form.reset();
+}
